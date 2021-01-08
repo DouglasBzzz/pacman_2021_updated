b19e269e3b49bd4cf27f242f26c706283c62eb23
Douglas <d.bianchezzi@gmail.com>
2021-01-08 10:13:02 -0300

    classe para desenhar o corpo daquela criatura possuida conhecida como PACMAN

--- pacman/pacman2.py
+++ pacman/pacman2.py
@@ -31,6 +31,23 @@ class Pacman:
         olho_raio = int(self.raio / 10)
         pygame.draw.circle(tela, PRETO, (olho_x, olho_y), olho_raio, 0)
 
+    def desenhar_espelhado(self, tela): #o espelhado é com a boca virada para a esquerda
+        # corpo
+        pygame.draw.circle(tela, AMARELO, (self.centro_x, self.centro_y), self.raio, 0)
+
+        #boca
+        canto_boca = (self.centro_x, self.centro_y)
+        labio_superior = (self.centro_x - self.raio, self.centro_y - self.raio)
+        labio_inferior = (self.centro_x - self.raio, self.centro_y)
+        pontos = [canto_boca, labio_superior, labio_inferior]
+        pygame.draw.polygon(tela, PRETO, pontos, 0)
+
+        #olhos
+        olho_x = int(self.centro_x - self.raio / 3)
+        olho_y = int(self.centro_y - self.raio * 0.70)
+        olho_raio = int(self.raio / 10)
+        pygame.draw.circle(tela, PRETO, (olho_x, olho_y), olho_raio, 0)
+
 if __name__ == '__main__':
     pacman = Pacman()
     while True:
